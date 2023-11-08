634f7a7713e5d00c54d8e587d0ab4c051ceb75db
15gk <gautamkriti89@gmail.com>
2023-11-08 22:14:41 +0545

    fixed minor error remove and edit is not needed

--- routes/slots.js
+++ routes/slots.js
@@ -4,26 +4,26 @@ var slots=require('../resources/slots')
 
 
 /* GET home page. */
-router.get('/add', function(req, res, next) {
+router.get('/add', async function(req, res, next) {
   res.render('addslots', { title: 'ADD slots' });
 });
-router.post('/save', function(req, res, next) {
-  slots.push({...req.body,_id:`00${slots.length}`})
+router.post('/save', async function(req, res, next) {
+  slots.push({...req.body,_id:`${slots.length}`,status:"booked"})
     res.redirect('/')
   });
-  router.get('/edit/:_id', function(req, res, next){
-    const slot = slots.find((slot)=>slot._id === req.params._id)
-    res.render('editslots', {title: "Edit slots", slot})
-  })
-  router.post('/saveEdited/:_id', function(req, res, next){
-    const currIndex = slots.findIndex((slot)=>slot._id === req.params._id)
-    // alert ("currIndex")
-    slots.splice(currIndex, 1, {...req.body, _id:req.params._id})
-    res.redirect('/')
-  })
+  // router.get('/edit/:_id', function(req, res, next){
+  //   const slot = slots.find((slot)=>slot._id === req.params._id)
+  //   res.render('editslots', {title: "Edit slots", slot})
+  // })
+  // router.post('/saveEdited/:_id', function(req, res, next){
+  //   const currIndex = slots.findIndex((slot)=>slot._id === req.params._id)
+  //   // alert ("currIndex")
+  //   slots.splice(currIndex, 1, {...req.body, _id:req.params._id})
+  //   res.redirect('/')
+  // })
 
 
-  router.get('/delete/:_id', function(req, res, next) {
+  router.get('/delete/:_id',async function(req, res, next) {
     const delIndex = slots.findIndex((slot) => slot._id === req.params._id);
     if (delIndex !== -1) {
         slots.splice(delIndex, 1);
@@ -33,4 +33,3 @@ router.post('/save', function(req, res, next) {
     res.redirect('/');
 });
 module.exports = router;
-  

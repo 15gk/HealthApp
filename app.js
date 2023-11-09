ab48bb2cbae5e7ef0e80d1fc105623ffcb01e580
Smreeta Shrestha <smreetashrestha1@gmail.com>
2023-11-09 11:23:38 +0545

    data fetched from json..not fetched from db

--- app.js
+++ app.js
@@ -1,56 +1,12 @@
-// var createError = require('http-errors')
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
-// var app = express();
-
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-// module.exports = app;
-
-
-
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const path = require('path'); // Import the path module
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost/doctor-appointment');
+ mongoose.connect('mongodb+srv://User:89YnLBCsPRfRiuyN@health.3svixrs.mongodb.net/?retryWrites=true&w=majority');
 
 // Middleware
 app.use(bodyParser.json());
@@ -60,38 +16,54 @@ const Appointment = mongoose.model('Appointment', {
   patientName: String,
   doctorName: String,
   date: Date,
+  isBooked: Boolean,
 });
 
+// Serve static files (HTML, CSS, JS, etc.) from the "public" directory
 app.use(express.static('public'));
 
-
-const Doctor = require('./resources/doctors'); // Import the Doctor model
-// Routes for managing appointments
-app.get('/appointments', async (req, res) => {
+// Route to get a list of doctors (you can customize this part)
+app.get('/doctors', async (req, res) => {
+  // Query the database to get a list of doctors
   try {
-    const appointments = await Appointment.find();
-    res.json(appointments);
+    // Mock list of doctors for this example...you can replace this with MongoDB data)
+    const doctors = [
+      {
+        _id: '1',
+        name: 'Dr. John Smith',
+        specialty: 'Cardiologist',
+        experience: '15 years',
+      },
+      // Add more doctor data here...
+    ];
+
+    res.json(doctors);
   } catch (error) {
-    res.status(500).json({ error: 'Error retrieving appointments' });
+    res.status(500).json({ error: 'Error retrieving doctors' });
   }
 });
 
-
-
-//code to handle the POST request from the form
+// Code to handle the POST request from the form to book an appointment
 app.post('/book-appointment', async (req, res) => {
   try {
-    // Get the selected date and time from the request body
-    const { date } = req.body;
+    const { date, patientName, doctorName } = req.body;
 
-// other patient information from the request body
-    const { patientName, doctorName } = req.body;
+    // Check if the selected time slot is already booked
+    const existingAppointment = await Appointment.findOne({
+      date: new Date(date),
+      isBooked: true,
+    });
 
-    // Creating  new patient appointment
+    if (existingAppointment) {
+      return res.status(400).json({ error: 'Time slot is already booked' });
+    }
+
+    // Create a new patient appointment
     const patientAppointment = new Appointment({
       patientName,
       doctorName,
-      date,
+      date: new Date(date),
+      isBooked: true,
     });
 
     // Save the patient appointment to the database
@@ -100,100 +72,63 @@ app.post('/book-appointment', async (req, res) => {
     // Respond with a success message
     res.status(200).json({ message: 'Appointment booked successfully' });
   } catch (error) {
-    // Handle errors (e.g., validation or database errors)
     console.error('Error booking appointment:', error);
     res.status(500).json({ error: 'Error booking appointment' });
   }
 });
 
-
-app.delete('/appointments/:id', async (req, res) => {
-  const appointmentId = req.params.id;
+// Route to check available time slots for a specific date
+app.get('/available-slots', async (req, res) => {
   try {
-    await Appointment.findByIdAndDelete(appointmentId);
-    res.json({ message: 'Appointment deleted successfully' });
+    // Parse the date from the query parameters
+    const date = new Date(req.query.date);
+
+    // Query the database to get available time slots for the given date
+    const availableSlots = await Appointment.find(
+      {
+        date: { $gte: date, $lt: new Date(date.getTime() + 24 * 60 * 60 * 1000) },
+        isBooked: false,
+      },
+      'date'
+    );
+
+    res.json(availableSlots);
   } catch (error) {
-    res.status(400).json({ error: 'Error deleting appointment' });
+    res.status(500).json({ error: 'Error retrieving available time slots' });
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Route to check booked appointments for a specific date
+app.get('/booked-appointments', async (req, res) => {
+  try {
+    // Parse the date from the query parameters
+    const date = new Date(req.query.date);
+
+    // Query the database to get booked appointments for the given date
+    const bookedAppointments = await Appointment.find(
+      {
+        date: { $gte: date, $lt: new Date(date.getTime() + 24 * 60 * 60 * 1000) },
+        isBooked: true,
+      },
+      'date'
+    );
+
+    res.json(bookedAppointments);
+  } catch (error) {
+    res.status(500).json({ error: 'Error retrieving booked appointments' });
+  }
 });
 
 // Define a route for the root ("/") URL
 app.get('/', (req, res) => {
-  res.send('Welcome to the Doctor Appointment System');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Define a route to handle requests for favicon.ico
 app.get('/favicon.ico', (req, res) => {
- 
   res.status(204).end(); // Send an empty response (No Content)
 });
 
-
-
-
-// Route to get available dates and time slots
-app.get('/available-dates', async (req, res) => {
-  const startDate = new Date(); // Start from today
-  const endDate = new Date();
-  endDate.setDate(startDate.getDate() + 30); // End date after 30 days
-
-  const availableDates = [];
-  const workingHours = { start: 9, end: 17 }; // Office working hours
-
-  for (let currentDate = new Date(startDate); currentDate <= endDate; currentDate.setDate(currentDate.getDate() + 1)) {
-    const dateSlots = [];
-    for (let hour = workingHours.start; hour < workingHours.end; hour++) {
-      for (let minute = 0; minute < 60; minute += 30) {
-        const slotDate = new Date(currentDate);
-        slotDate.setHours(hour, minute, 0, 0);
-        dateSlots.push(slotDate);
-      }
-    }
-    availableDates.push({ date: currentDate, timeSlots: dateSlots });
-  }
-
-  //comapring with database query
-  const bookedAppointments = await Appointment.find({ date: { $gte: startDate, $lte: endDate } });
-
-  // Filtering out booked slots
-  for (const appointment of bookedAppointments) {
-    const { date } = appointment;
-    availableDates.forEach((availableDate) => {
-      if (availableDate.date.toDateString() === date.toDateString()) {
-        availableDate.timeSlots = availableDate.timeSlots.filter(
-          (slot) => slot.getTime() !== date.getTime()
-        );
-      }
-    });
-  }
-
-  // Saving the available slots to the database
-  try {
-    for (const slot of availableDates) {
-      const availableSlot = new AvailableSlots({
-        date: slot.date,
-        timeSlots: slot.timeSlots,
-      });
-      await availableSlot.save();
-    }
-    res.json(availableDates);
-  } catch (error) {
-    res.status(500).json({ error: 'Error saving available slots' });
-  }
-});
-
-
-
-app.get('/doctors', async (req, res) => {
-  try {
-    // Querying the database to get a list of doctors
-    const doctors = await Doctor.find({}, 'name'); // from Doctor model that has a 'name' field
-    res.json(doctors);
-  } catch (error) {
-    res.status(500).json({ error: 'Error retrieving doctors' });
-  }
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
--- resources/doctors.js
+++ resources/doctors.js
@@ -1,42 +1,47 @@
 let doctors = [
     {
-      "_id": "D001",
+      "_id": "1",
+      "doctorid":"d1",
       "name": "Dr. John Smith",
       "specialty": "Cardiologist",
    
       "experience": "15 years",
     },
     {
-      "_id": "D002",
-      "doctorid":"",
+      "_id": "2",
+      "doctorid":"d2",
       "name": "Dr. Sarah Johnson",
       "specialty": "Pediatrician",
    
       "experience": "10 years",
     },
     {
-      "_id": "D003",
+      "_id": "3",
+      "doctorid":"d3",
       "name": "Dr. Michael Wilson",
       "specialty": "Dermatologist",
    
       "experience": "12 years",
     },
     {
-      "_id": "D004",
+      "_id": "4",
+      "doctorid":"d4",
       "name": "Dr. Emily Brown",
       "specialty": "Ophthalmologist",
       
       "experience": "8 years",
     },
     {
-      "_id": "D005",
+      "_id": "5",
+      "doctorid":"d5",
       "name": "Dr. Lisa Davis",
       "specialty": "Orthopedic Surgeon",
    
       "experience": "18 years",
     },
     {
-      "_id": "D006",
+      "_id": "6",
+      "doctorid":"d6",
       "name": "Dr. Robert White",
       "specialty": "Psychiatrist",
      
